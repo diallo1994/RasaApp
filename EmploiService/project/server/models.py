d019bcfe9d4caab41258d3571d8001016bb2af55
VINCE-CTS <vincent@ctsfares.com>
2019-03-20 00:39:18 +0100

    adding Secteur & Emplois classes

--- EmploiService/project/server/models.py
+++ EmploiService/project/server/models.py
@@ -97,3 +97,14 @@ class BlacklistToken(db.Model):
             return True  
         else:
             return False
+class Secteur(db.Model):
+    __tablename__ = "secteur"
+    idsecteur=db.Column(db.Integer,primary_key=True, autoincrement=True)
+    nomsecteur=db.Column(db.String(255), unique=False, nullable=False)
+    metier=db.relationship('Metier', backref='secteur', lazy='true')
+
+class Emplois(db.Model):
+    __tablename__ = "emplois"
+    idemploi=db.Column(db.Integer,primary_key=True, autoincrement=True)
+    nomemploi=db.Column(db.String(255), unique=False, nullable=False)
+    offres=db.relationship('Offres', backref='emplois', lazy='true')
